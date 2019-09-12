d90ecf31c30b14b5d5487f648c62b400e00ad648
Vladymyr Strizhov <bud.strizhov@gmail.com>
2019-09-13 01:28:21 +0300

    Updates

---
+++ precache-manifest.8e2db84b4746327774f1fdc9b3532dac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5712a2d19576be46c5047951101919",
+    "url": "/udemy_monsters/index.html"
+  },
+  {
+    "revision": "3cc51af4cb243744cf4b",
+    "url": "/udemy_monsters/static/css/main.b2ec3073.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/udemy_monsters/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "3cc51af4cb243744cf4b",
+    "url": "/udemy_monsters/static/js/main.c2473816.chunk.js"
+  },
+  {
+    "revision": "cdffa9a20da0f4440d17",
+    "url": "/udemy_monsters/static/js/runtime~main.c4139138.js"
+  }
+]);
